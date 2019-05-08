ecea1fa618c500951e133a8e69a77901b2bbbd39
Tenzin <tenzinball@icloud.com>
2019-05-08 14:49:21 +0100

    setting default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,8 @@
 require "oystercard"
 
 describe Oystercard do
+  it "checks that the new card has a balance of 0 by default" do
+    card = Oystercard.new
+    expect(card.balance).to eq 0
+  end
 end
